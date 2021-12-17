0f2d51c47f2ebcb637fdfe35c9abffa27435b738
SHAHULDEV22 <79395102+SHAHULDEV22@users.noreply.github.com>
2021-12-17 15:15:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <a href="/">home</a>
+      <a href="google.com">home</a>
     </div>
   );
 }
